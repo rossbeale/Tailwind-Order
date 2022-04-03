a814ffa5c6ecfd1c37fe97c3b4c861fbaa116837
Ross Beale <rossbeale@gmail.com>
2022-04-03 20:00:14 +0100

    Update tailwind-order.py

--- tailwind-order.py
+++ tailwind-order.py
@@ -46,3 +46,8 @@ class TailwindOrderCommand(sublime_plugin.TextCommand):
                 sorted_class += ' '.join(sorted(other_classes))
             self.view.replace(edit, region, sorted_class)
             dif += len(sorted_class) - len(str(self.view.substr(item)))
+
+class CommandOnSave(sublime_plugin.EventListener):
+
+    def on_pre_save(self, view):
+        view.run_command('tailwind_order')
